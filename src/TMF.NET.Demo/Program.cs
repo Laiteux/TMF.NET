4656b7f2695c6629d4f7de49bf8feefc09039c5d
Laiteux <matt@laiteux.dev>
2023-04-26 20:14:59 +0200

    Improve Demo program comments

--- src/TMF.NET.Demo/Program.cs
+++ src/TMF.NET.Demo/Program.cs
@@ -1,19 +1,19 @@
 ﻿// This demo project will:
 // - Connect to an account
-// - Retrieve some info including the linked email address and owned coppers
-// - Validate player key (per-session requirement for spending coppers)
-// - Send a message to a player, including an optional donation
+// - Retrieve some info, including the account's linked email address and owned copper amount
+// - Submit, validate, associate player key (per-session requirement for spending coppers)
+// - Send an in-game private message to a player, including an optional donation
 
 using TMF.NET;
 
 const string Login = "19191"; // Account login
 const string Password = "19191"; // Account password
 const string PlayerKey = "5EC"; // Only the last 3 characters are required for spending coppers
-const string Recipient = "leak"; // Login of the player to send a message to (will cost 2 coppers)
+const string Recipient = "leak"; // Login of the player to send a private message to (will cost 2 coppers)
 const int Donation = 0; // Amount of coppers to send along with the message (will cost an extra 5% Nadeo fee)
 
 // First of all, instantiate a GameApi object - used to interact with... well, the game API
-// If you wish, you can also pass a IWebProxy object for it to use
+// If you wish, you can also pass a IWebProxy object in the constructor for it to use
 var gameApi = new GameApi();
 
 // Open a new session using the account's login
@@ -21,17 +21,17 @@ var gameApi = new GameApi();
 // Returns a unique ID and a salt, which is being used for hashing values (not to worry about, as TMF.NET handles all of that for you behind the scenes!)
 var openSession = await gameApi.OpenSessionAsync(Login);
 
-// Connect with our previously created session, and a password
+// Connect with our previously created session, and the associated account's password
 // If the password is correct, our session ID will then become authenticated server-side, and we will be able to use this GameSession object to proceed further
 var gameSession = await gameApi.ConnectAsync(openSession, Password);
 
-Console.WriteLine($"Logged in as {Login}");
+Console.WriteLine($"Successfully logged in as {Login}");
 Console.WriteLine($"Session ID: {gameSession.SessionId}");
 Console.WriteLine();
 
 try
 {
-    // Retrieve some info about the account online profile (ranking, location, email, coppers...)
+    // Retrieve some info about the account's online profile (ranking, location, email, coppers...)
     // I have only included a few values I considered relevant, but way more data is actually available
     // If you wish, you could create your own custom response class with more values by making it implement GetOnlineProfileResponse or even just ResponseBase
     // Then, you could use it to send a GetOnlineProfileRequest and retrieve the data you're looking for, just like that:
@@ -57,20 +57,21 @@ try
     if (Donation != 0 && maxSpendableCoppers < Donation)
         throw new("Not enough Coppers - Message not sent.");
 
-    // Submit, validate, associate player key with our session - required for spending coppers
-    // In order to avoid this extra request, you can also pass the key to the ConnectAsync method right away
+    // Submit, validate, associate player key with our session - this is required for spending coppers
+    // In order to avoid this extra request, you could also pass the key directly as a parameter to the ConnectAsync method
     if (!await gameApi.ValidatePlayerKeyAsync(gameSession, PlayerKey))
         throw new("Invalid player key.");
 
     // Send a message to a player
-    // Here we are only setting a subject and donation, but we could obviously set a message body as well
+    // Here we are only setting a subject and donation, but we could obviously set a message body (text) as well
     await gameApi.SendMessageAsync(gameSession, Recipient, "Hello from TMF.NET (:", null, Donation);
 
     // That's it for the demo!
     // I really hope you enjoy this library, as I did my best to make it as elegant and easy-to-use as possible
-    // If you do then please consider Starring the repo on GitHub, that would be very much appreciated!
+    // If you do then please consider Starring the repo on GitHub, that would be very much appreciated! <3
     Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine($"Done, message sent to {Recipient}");
+    Console.WriteLine($"Successfully sent message to {Recipient}");
+    Console.Write("Done ");
 }
 catch (Exception ex)
 {
@@ -81,7 +82,7 @@ finally
 {
     Console.ResetColor();
 
-    // Do not forget to disconnect a session once you're done
+    // Do not forget to disconnect a session once you are done with it
     // This is required if you wish to login to the same account later, as the game might throw an "already connected" error otherwise
     await gameApi.DisconnectAsync(gameSession);
 }
